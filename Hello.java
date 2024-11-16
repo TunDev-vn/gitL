60179a48fcd4ca2b2f640ede8a3964ab73319f16
HoangTung <tungvanzk03@gmail.com>
2024-11-17 00:33:11 +0700

    first commit restored

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package GitLearning;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World 2 update");
+        System.out.println("Hello World");
     }
 }
